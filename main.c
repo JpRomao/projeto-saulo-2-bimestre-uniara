a8aead689abc8f65f1fec9b1277c2550156b3054
Bia <fantini.bia@gmail.com>
2023-05-05 00:09:46 -0300

    Adicionado funções salário, idade, nome

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 typedef struct Funcionario
 {
@@ -7,6 +9,45 @@ typedef struct Funcionario
   int categoria;
   int idade;
 } Reg;
+
+char *cadastraNome()
+{
+
+  char *nome = malloc(20 * sizeof(char));
+
+  printf("Digite o nome do funcionario: ");
+  fgets(nome, 20, stdin);
+  nome[strcspn(nome, "\r\n")] = 0;
+
+  return nome;
+}
+
+int cadastraIdade()
+{
+  int idade;
+
+  do
+  {
+    printf("Digite a idade do funcionario: ");
+    scanf("%d", &idade);
+
+    if (idade <= 0)
+    {
+      printf("A idade digitada eh invalida! Digite novamente.\n");
+    }
+  } while (idade <= 0);
+  return idade;
+}
+
+double cadastraSalario()
+{
+  double salario;
+
+  printf("Digite o salario do funcionario: ");
+  scanf("%lf", &salario);
+
+  return salario;
+}
 
 int main()
 {
