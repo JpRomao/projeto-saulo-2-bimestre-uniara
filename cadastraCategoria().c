f8869ebea3503dca70f834ac3a10a89107ddcf98
Matheus-Estrella <matheus_estrella@hotmail.com>
2023-05-07 15:56:00 -0300

    Cadastro de categoria para funcao de cadastrar usuarios

---
+++ cadastraCategoria().c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct Funcionario
+{
+    char nome[20];
+    double salario;
+    int categoria;
+    int idade;
+} Reg;
+
+int cadastraCategoria()
+{
+    int categoria;
+
+    printf("Digite a qual categoria o funcionario sera classificado\n 1- Gerencia\n 2- Supervisao\n 3- Operacional\n R:  ");
+    scanf("%d", &categoria);
+    switch (categoria)
+    {
+    case 1:
+        printf("\nVoce cadastrou um funcionario no setor de gerencia");
+        break;
+    case 2:
+        printf("\nVoce cadastrou um funcionario no setor de supervisao");
+        break;
+    case 3:
+        printf("\nVoce cadastrou um funcionario no setor operacional");
+        break;
+    }
+    return categoria;
+}
+
+int main()
+{
+    int categoria = cadastraCategoria();
+    printf("Categoria: %d\n", categoria);
+
+    return 0;
+}
