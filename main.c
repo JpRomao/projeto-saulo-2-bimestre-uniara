7e3189118037a59fc7ff029c3141014bf1b47e08
Bia <fantini.bia@gmail.com>
2023-05-07 21:41:24 -0300

    Acrescentei o arquivo cadastra categoria ao arquivo main e no main criei a funçao cadastra funcionario.

--- main.c
+++ main.c
@@ -49,7 +49,50 @@ double cadastraSalario()
   return salario;
 }
 
+int cadastraCategoria()
+{
+  int categoria;
+
+  printf("Digite a qual categoria o funcionario sera classificado\n 1- Gerencia\n 2- Supervisao\n 3- Operacional\n R:  ");
+  scanf("%d", &categoria);
+  switch (categoria)
+  {
+  case 1:
+    printf("\nVoce cadastrou um funcionario no setor de Gerencia.\n");
+    break;
+  case 2:
+    printf("\nVoce cadastrou um funcionario no setor de Supervisao.\n");
+    break;
+  case 3:
+    printf("\nVoce cadastrou um funcionario no setor Operacional.\n");
+    break;
+  }
+  return categoria;
+}
+
+Reg criarFuncionario()
+{
+  Reg funcionario;
+
+  strcpy(funcionario.nome, cadastraNome());
+  funcionario.categoria = cadastraCategoria();
+  funcionario.salario = cadastraSalario();
+  funcionario.idade = cadastraIdade();
+
+  printf("Funcionario criado com sucesso!\n");
+
+  return funcionario;
+}
+
 int main()
 {
+  Reg funcionario = criarFuncionario();
+
+  printf("\n--------- Funcionario Cadastrado ----------\n");
+  printf("Nome: %s\n", funcionario.nome);
+  printf("Categoria: %d\n", funcionario.categoria);
+  printf("Salario: %.2lf\n", funcionario.salario);
+  printf("Idade: %d\n", funcionario.idade);
+
   return 0;
 }
